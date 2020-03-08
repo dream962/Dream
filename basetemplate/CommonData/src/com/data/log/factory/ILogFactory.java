726f03852a1ca942048d068a81f1db965c4065cc
dream <342370472@qq.com>
2020-03-09 00:09:44 +0800

    日志
    日志

---
+++ basetemplate/CommonData/src/com/data/log/factory/ILogFactory.java
@@ -0,0 +1,13 @@
+package com.data.log.factory;
+
+import com.base.database.DBPoolComponent;
+import com.base.database.DatabaseType;
+import com.base.database.pool.DBHelper;
+
+public interface ILogFactory
+{
+    /** 日志库 */
+    static final DBHelper logHelper = DBPoolComponent.getDBHelper(DatabaseType.DB_LOG);
+
+    public void save();
+}
